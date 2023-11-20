210c18cdeed003ff31668da1a88f8f792dd82554
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-20 13:36:29 -0500

    Add env file to hide API key

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,15 @@
-// const getNewsArticles = async () => {
-//   const response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=cb8fd1526dfe49a9bb26987581f25e22")
-//   if(!response.ok) {
-//     throw new Error(`${response.status} ${response.statusText}`)
-//   }
+const getNews = async () => {
+  const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
 
-//   let news = await response.json()
-//   return news
-// }
+  console.log("apiKey", apiKey)
+  const response = await fetch(url)
+  if(!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}`)
+  }
 
-// export default getNewsArticles
+  let news = await response.json()
+  return news
+}
+
+export default getNews
