1e7ee8f79762d60c5ef5e36d4052a3b64c267a05
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-23 11:25:56 -0500

    Add article detail view

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -2,19 +2,20 @@ import "./ArticleDetails.css"
 import { useParams } from "react-router-dom"
 
 function ArticleDetails({allArticles}) {
-  console.log("inside art details", allArticles)
-
-  const {id}= useParams()
-
-  const locatedArticle = () => {
-    return allArticles.find((article) => article.publishedAt === id)
-  }
-
-// console.log("found", locatedArticle())
+  const articleID = useParams().id
+  const clickedArticle = allArticles.find((article) => article.publishedAt === articleID)
+  console.log("click", clickedArticle)
 
   return (
-    <div>
-    </div>
+    <section className="article-details-container">
+      <section className="detail-headline-and-img">
+        <h2 className="detail-headline">{clickedArticle.title}</h2>
+        <img className="detail-image" src={clickedArticle.urlToImage} alt={`Image of ${clickedArticle.title}`}/>
+      </section>
+      <article className="detail-content">
+        <p>{clickedArticle.content?.split('[')[0]}<a href={clickedArticle.url}>{`read on at ${clickedArticle.source.name}.`}</a></p>
+      </article>
+    </section>
   )
 }
 
