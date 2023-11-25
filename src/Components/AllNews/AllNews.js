ebab9f75ebb0ddb6563fd952e33f9609974d77df
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-25 09:17:35 -0500

    Add proptypes

--- src/Components/AllNews/AllNews.js
+++ src/Components/AllNews/AllNews.js
@@ -1,8 +1,9 @@
 import "./AllNews.css"
 import Article from "../Article/Article"
+import PropTypes from "prop-types"
 
 function AllNews({allArticles}) {
-  console.log("all", allArticles)
+  // console.log("all", allArticles)
   const articleCard = allArticles.map((article, index) => {
     return <Article key={article.publishedAt} article={article} index={allArticles.indexOf(article)}/>
   })
@@ -12,3 +13,18 @@ function AllNews({allArticles}) {
 }
 
 export default AllNews
+AllNews.propTypes = {
+  allArticles: PropTypes.arrayOf(
+    PropTypes.shape({
+      author: PropTypes.string,
+      content: PropTypes.string,
+      description: PropTypes.string,
+      publishedAt: PropTypes.string,
+      source: PropTypes.object,
+      title: PropTypes.string,
+      url: PropTypes.string,
+      urlToImage: PropTypes.string
+    }).isRequired
+  )
+}
+
