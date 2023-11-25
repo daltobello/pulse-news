87f13d5ee72ca828dd28294d9cca597d7a5eb6cc
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-11-25 18:07:14 -0500

    Test article details nav

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -20,14 +20,30 @@ describe('homepage', () => {
         cy.get('@articleContainer').find(".article-headline").contains("Israel says soldier executed, foreign hostages held at Gaza's Shifa hospital - Reuters")
         cy.get('@articleContainer').find(".article-description").contains("Israel stepped up accusations of Hamas abuses at the Gaza Strip's biggest hospital on Sunday, saying a captive soldier had been executed and two foreign hostages held at a site that has been a focus of its devastating six-week-old offensive.")
       })
-      .get('.articles-wrapper .article-link-wrapper').first().find(".article-image").should('exist')
+      .get('.articles-wrapper .article-link-wrapper').first().find(".article-image").should('be.visible')
       .get('.articles-wrapper .article-link-wrapper').last().within(() => {
         cy.get(".article-container").as('articleContainer')
         cy.get('@articleContainer').find(".article-date").contains("Published on: 19-11-2023")
         cy.get('@articleContainer').find(".article-headline").contains("Patrick Mahomes Says Taylor Swift and Travis Kelce's Romance is a 'Huge Deal' - Entertainment Tonight")
         cy.get('@articleContainer').find(".article-description").contains("The Kansas City Chiefs quarterback says their relationship is not a distraction.")
       })
-      .get('.articles-wrapper .article-link-wrapper').last().find(".article-image").should('exist')
+      .get('.articles-wrapper .article-link-wrapper').last().find(".article-image").should('be.visible')
       .click()
+      .url().should('eq', 'http://localhost:3000/article/4')
+      .get('h1').contains("Pulse News")
+      .get('.detail-headline-and-img .detail-headline').contains("Patrick Mahomes Says Taylor Swift and Travis Kelce's Romance is a 'Huge Deal' - Entertainment Tonight")
+      .get('.detail-headline-and-img .detail-image').should("be.visible")
+      .get('.article-content').contains("Patrick Mahomes is following Taylor Swift and Travis Kelce's love story! On Sunday, the Kansas City Chiefs quarterback sat down with ESPN ahead of the team's rematch game against the Philadelphia E… read on at Entertainment Tonight.")
+      .get('.source-link').contains("read on at Entertainment Tonight.")
   })
+
+  // it("should handle server error", () => {
+  //   cy.intercept("GET", "https://newsapi.org/v2/top-headlines?country=us&apiKey=cb8fd1526dfe49a9bb26987581f25e22", {
+  //     statusCode: 400,
+  //     body: ""
+  //   })
+  //   .as("getAllNews")
+  //   .visit('http://localhost:3000')
+  //   cy.wait("@getAllNews")
+  // })
 })
